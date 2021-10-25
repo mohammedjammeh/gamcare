0d9131e79246274b459ea52230ea46a991b87001
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-10-25 16:29:34 +0100

    WIP: setting up login page structure and stylings

--- src/main/java/com/projects/gamcare/controllers/Login.java
+++ src/main/java/com/projects/gamcare/controllers/Login.java
@@ -2,13 +2,27 @@ package com.projects.gamcare.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Login {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label title;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private TextField usernameText;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private TextField passwordText;
+
+    @FXML
+    protected void onLoginButtonClick() {
+        System.out.println("You are now logged in.");
     }
 }
