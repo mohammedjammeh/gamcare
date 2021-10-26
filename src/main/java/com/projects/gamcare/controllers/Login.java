0b13c9e197c3ec068332f4d22c0d3c27a1ed0df0
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-10-26 13:59:56 +0100

    further style the login and add error text for failed authentication.

--- src/main/java/com/projects/gamcare/controllers/Login.java
+++ src/main/java/com/projects/gamcare/controllers/Login.java
@@ -3,26 +3,40 @@ package com.projects.gamcare.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class Login {
 
     @FXML
-    private Label title;
+    private Label titleLabel;
+
+    @FXML
+    private VBox errorBox;
+
+    @FXML
+    private Label errorLabel;
 
     @FXML
     private Label usernameLabel;
 
     @FXML
-    private TextField usernameText;
+    private TextField usernameTextField;
 
     @FXML
     private Label passwordLabel;
 
     @FXML
-    private TextField passwordText;
+    private TextField passwordTextField;
+
+    @FXML void initialize() {
+        errorBox.setVisible(false);
+        errorBox.setManaged(false);
+    }
 
     @FXML
     protected void onLoginButtonClick() {
+        errorBox.setVisible(true);
+        errorBox.setManaged(true);
         System.out.println("You are now logged in.");
     }
 }
