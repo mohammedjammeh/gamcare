7a3d9b88463e9ca870c3eaa8b20d09a1ce33c8b8
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-10-26 12:30:09 +0100

    Change project title from GamCare to Gamcare

--- src/main/java/com/projects/gamcare/Main.java
+++ src/main/java/com/projects/gamcare/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
-        stage.setTitle("GamCare");
+        stage.setTitle("Gamcare");
         stage.setScene(scene);
         stage.show();
     }
