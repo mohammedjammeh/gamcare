2d2f5b6b54ca75e56fed2c9964a589efe44b33cb
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-11-04 18:55:31 +0000

    WIP: create patient view

---
+++ src/main/java/com/projects/gamcare/controllers/Patient.java
@@ -0,0 +1,63 @@
+package com.projects.gamcare.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Patient {
+    @FXML
+    private Label firstNameFieldLabel;
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private Label lastNameFieldLabel;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private Label emailFieldLabel;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private Label dateOfBirthFieldLabel;
+
+    @FXML
+    private DatePicker dateOfBirthPicker;
+
+    @FXML
+    private Label compoundFieldLabel;
+
+    @FXML
+    private TextField compoundTextField;
+
+    @FXML
+    private Label regionFieldLabel;
+
+    @FXML
+    private Label addressDescriptionFieldLabel;
+
+    @FXML
+    private TextArea addressDescriptionTextArea;
+
+    @FXML
+    protected void onHospitalsButtonClick() {
+        System.out.println("You are now looking at hospitals.");
+    }
+
+    @FXML
+    protected void onMyProfileButtonClick() {
+        System.out.println("You are now looking at your profile.");
+    }
+
+    @FXML
+    protected void onLogOutButtonClick() {
+        System.out.println("You are now logged out.");
+    }
+}
