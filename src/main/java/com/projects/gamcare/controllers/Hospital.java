bf3adcb55605622cfd4457b596771f9a0bd4c06e
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-11-04 16:40:35 +0000

    Complete designing the hospital show page

--- src/main/java/com/projects/gamcare/controllers/Hospital.java
+++ src/main/java/com/projects/gamcare/controllers/Hospital.java
@@ -19,6 +19,8 @@ public class Hospital {
         System.out.println("You are now logged out.");
     }
 
+
+
     // Index
     @FXML
     protected void onAddHospitalButtonClick() {
@@ -30,6 +32,9 @@ public class Hospital {
         System.out.println("You can now see a hospital.");
     }
 
+
+
+
     // Show
     @FXML
     protected void onAddPatientButtonClick() {
@@ -39,5 +44,37 @@ public class Hospital {
     @FXML
     protected void onShowPatientButtonClick() {
         System.out.println("You can now see a patient.");
+    }
+
+    @FXML
+    protected void onEditPatientButtonClick() {
+        System.out.println("You can now edit a patient.");
+    }
+
+    @FXML
+    protected void onDeletePatientButtonClick() {
+        System.out.println("You can now delete a patient.");
+    }
+
+
+
+    @FXML
+    protected void onAddDoctorButtonClick() {
+        System.out.println("You have now added a doctor.");
+    }
+
+    @FXML
+    protected void onShowDoctorButtonClick() {
+        System.out.println("You can now see a doctor.");
+    }
+
+    @FXML
+    protected void onEditDoctorButtonClick() {
+        System.out.println("You can now edit a doctor.");
+    }
+
+    @FXML
+    protected void onDeleteDoctorButtonClick() {
+        System.out.println("You can now delete a doctor.");
     }
 }
