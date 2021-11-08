ef7d701678d23eda2fa22aeeba48857217c3b626
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-11-08 11:07:30 +0000

    Small styling adjustments to hospital show  and create user view

--- src/main/java/com/projects/gamcare/Main.java
+++ src/main/java/com/projects/gamcare/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/hospital/index.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/hospital/show.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/patient/create.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/hospital/show.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/patient/create.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
         stage.setTitle("Gamcare");
         stage.setScene(scene);
