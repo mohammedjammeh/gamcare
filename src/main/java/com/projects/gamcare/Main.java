a267403cb59811e8e0c05ef55f26f1be37c8e424
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-11-19 10:19:59 +0000

    Set up and connect to local database, also add commented CRUD query statements in the main file to be used in the future.

--- src/main/java/com/projects/gamcare/Main.java
+++ src/main/java/com/projects/gamcare/Main.java
@@ -6,11 +6,59 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login.fxml"));
+        try {
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamcare", "root", "3aj3!96wMWeyU9&z");
+
+            Statement myStmt = myConn.createStatement();
+
+//            SELECT
+//            ResultSet myRs = myStmt.executeQuery("SELECT * FROM users");
+//
+//            while (myRs.next()) {
+//                System.out.println(myRs.getString("first_name"));
+//            }
+
+
+
+
+//            INSERT
+//            String insertSql = "insert into users"
+//                    + "(title, first_name, middle_name, last_name, email, phone_number, relevant_link, date_of_birth, gender, compound, town, region, other_details)"
+//                    + "values(1, 'Omar', 'Bailo', 'Jallow', 'bailo@gmail.com', '07506259330', 'https://bailo.com/', '1986-08-21', 1, 'Jallow kunda', 'Brikama', 1, 'I am chilling')";
+//
+//            myStmt.executeUpdate(insertSql);
+
+
+
+
+//            UPDATE
+//            String updateSql = "update users set middle_name='Musa' where id=2";
+//
+//            myStmt.executeUpdate(updateSql);
+
+
+
+
+//            DELETE
+//            String deleteSql = "delete from users where email='bailo@gmail.com'";
+//
+//            int rowsAffected = myStmt.executeUpdate(deleteSql);
+//
+//            System.out.println("Number of rows affected: " + rowsAffected);
+
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login.fxml"));
 
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/hospital/index.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/hospital/create.fxml"));
@@ -18,7 +66,7 @@ public class Main extends Application {
 
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/doctor/index.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/doctor/create.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/doctor/show.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/doctor/show.fxml"));
 
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/patient/create.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/patient/show.fxml"));
