bfef575addc07af2efb03a2e21038dd4970f6d4a
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-11-20 10:11:33 +0000

    Comment out the whole database try catch block on the main java file

--- src/main/java/com/projects/gamcare/Main.java
+++ src/main/java/com/projects/gamcare/Main.java
@@ -11,8 +11,8 @@ import java.sql.*;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        try {
-            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamcare", "root", "3aj3!96wMWeyU9&z");
+//        try {
+//            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamcare", "root", "3aj3!96wMWeyU9&z");
 
 //            // PREPARE STATMENT
 //            String selectSql = "SELECT * FROM users WHERE email = ?";
@@ -69,11 +69,11 @@ public class Main extends Application {
 //
 //            System.out.println("Number of rows affected: " + rowsAffected);
 
-        }
-        catch (Exception exception)
-        {
-            exception.printStackTrace();
-        }
+//        }
+//        catch (Exception exception)
+//        {
+//            exception.printStackTrace();
+//        }
 
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login.fxml"));
