0d0516157924a74611d2abc562de61b87e451da9
mohammedjammeh <mohammedjammeh@yahoo.com>
2021-12-01 09:49:55 +0000

    Add basic passing hashing/salting functionality to the patient create controller.

--- src/main/java/com/projects/gamcare/controllers/patient/Create.java
+++ src/main/java/com/projects/gamcare/controllers/patient/Create.java
@@ -1,11 +1,13 @@
 package com.projects.gamcare.controllers.patient;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class Create {
     @FXML
@@ -128,15 +130,54 @@ public class Create {
 
 
 
+    @FXML
+    private Button createPatientButton;
+
+
+
     @FXML void initialize() {
         errorBox.setVisible(false);
         errorBox.setManaged(false);
     }
 
     @FXML
-    protected void onCreatePatientButtonClick() {
-        errorBox.setVisible(true);
-        errorBox.setManaged(true);
-        System.out.println("There is an error.");
+    protected void onCreatePatientButtonClick() throws NoSuchAlgorithmException {
+
+
+        String data = "Hello World";
+        String algorithm = "SHA-256";
+        byte[] salt = createSalt();
+
+        System.out.println(generateHash(data, algorithm, salt));
     }
+
+    public static byte[] createSalt() {
+        byte[] bytes = new byte[20];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(bytes);
+
+        return bytes;
+    }
+
+    public static String generateHash(String data, String algorithm, byte[] salt) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance(algorithm);
+        digest.reset();
+        digest.update(salt);
+        byte[] hash = digest.digest(data.getBytes());
+
+        return bytesToHex(hash);
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+
+        return new String(hexChars, StandardCharsets.UTF_8);
+    }
+
+    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
 }
